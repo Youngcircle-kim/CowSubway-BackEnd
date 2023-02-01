11c26776ee081d8d9e261dce801aadde27082618
Youngcircle-kim <108862575+Youngcircle-kim@users.noreply.github.com>
2023-02-01 16:53:25 +0900

    init; prettier 재설정

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-import express from 'express'
+import express from 'express';
 
-const app = express()
+const app = express();
 
-const port = 3000
+const port = 3000;
 
 app
   .get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World!');
   })
   .listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+    console.log(`Example app listening on port ${port}`);
+  });

6c9a1ef285f5e585392cecd776c79e6b001ffb16
gogun <rhrjs0131@naver.com>
2023-02-16 13:46:55 +0900

    feat; dotenv 추가

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-
+const dotenv = require('dotenv');
+dotenv.config();
+console.log(process.env.DB_PASSWORD);
 const app = express();
 
 const corOptions = {
